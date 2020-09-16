ce087126ecd41748b6f321bd36cbfc3ad47b9f51
Mexion <1095705440@qq.com>
2020-09-17 03:08:34 +0800

    feat(all): initial commit
    create the project by create-next-app && do some initial configuration

---
+++ pages/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "../styles/Home.module.scss";
+
+export default function Home(): JSX.Element {
+  return <div className={styles.container}>Sprout</div>;
+}
