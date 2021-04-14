3216ca3cef33fe6cfefaf89cc7cd76ea6aa6353b
Mexion <1095705440@qq.com>
2021-04-15 01:09:28 +0800

    build: delete the webpack5 option in next

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPWA({
-  future: {
-    webpack5: true,
-  },
   pwa: {
     dest: 'public',
     runtimeCaching,
