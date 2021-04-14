3985008e6949633752ffd0e5505dcd3ef7b22b8e
Mexion <1095705440@qq.com>
2021-04-15 01:27:34 +0800

    ci: try to fix the next.js build

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
-const withPWA = require('next-pwa');
-const runtimeCaching = require('next-pwa/cache');
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
+  future: {
+    webpack5: true,
+  },
   pwa: {
-    dest: 'public',
+    dest: "public",
     runtimeCaching,
     disable: process.env.NODE_ENV === "development",
   },
