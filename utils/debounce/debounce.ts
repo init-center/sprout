8644d15b87d799873058c3ac500e3dc512af4bdc
Mexion <1095705440@qq.com>
2021-04-19 00:11:33 +0800

    style: replace some static files

--- utils/debounce/debounce.ts
+++ utils/debounce/debounce.ts
@@ -1,6 +1,6 @@
 export const debounce = (func: (...args: unknown[]) => unknown, wait = 500) => {
-  let timer: ReturnType<typeof setTimeout> = null;
-  return function (...args: unknown[]) {
+  let timer: ReturnType<typeof setTimeout> | null = null;
+  return (...args: unknown[]) => {
     if (timer) clearTimeout(timer);
     timer = setTimeout(() => {
       func.apply(this, args);
