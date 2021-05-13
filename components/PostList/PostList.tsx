5b14dba6ef523879d01ba46525c6db3bf0ad0445
Mexion <1095705440@qq.com>
2021-05-14 01:18:20 +0800

    fix: try fix repeat post in list

--- components/PostList/PostList.tsx
+++ components/PostList/PostList.tsx
@@ -21,45 +21,42 @@ const PostList: FC<PostListProps> = memo(({ postList, queryFields = {} }) => {
   const [posts, setPosts] = useState(postList);
 
   const fetchPosts = useCallback(
-    (queryFields: PostListQueryFields) => {
-      (async () => {
-        if (isFetching) {
-          return;
-        }
-        const { page = 1, limit = 5 } = queryFields;
-        if (page < 1 || limit < 1 || posts.list.length >= posts.page.count) {
-          return;
-        }
-        if (isFetching) return;
-        try {
-          setIsFetching(true);
-          const result = await http.get<ResponseData<PostListType>>(`/posts`, {
-            params: { page, limit, ...queryFields },
+    async (queryFields: PostListQueryFields) => {
+      if (isFetching) {
+        return;
+      }
+      const { page = 1, limit = 5 } = queryFields;
+      if (page < 1 || limit < 1 || posts.list.length >= posts.page.count) {
+        return;
+      }
+      try {
+        setIsFetching(true);
+        const result = await http.get<ResponseData<PostListType>>(`/posts`, {
+          params: { page, limit, ...queryFields },
+        });
+        if (result.status === 200 && result.data.code === 2000) {
+          const postList = result.data.data;
+          const currentLastIndex = posts.list.length - 1;
+          setPosts({
+            page: postList.page,
+            list: [...posts.list, ...postList.list],
           });
-          if (result.status === 200 && result.data.code === 2000) {
-            const postList = result.data.data;
-            const currentLastIndex = posts.list.length - 1;
-            setPosts({
-              page: postList.page,
-              list: [...posts.list, ...postList.list],
-            });
-            setIsFetching(false);
-            setTimeout(() => {
-              const newFirst = document.querySelector(
-                `.${styles.posts} div:nth-child(${currentLastIndex + 1})`
-              );
-              scrollToElement(newFirst);
-            });
-          }
-        } catch (err) {
           setIsFetching(false);
-          const msg = err?.response?.data?.message;
-          if (message && msg) {
-            message.destroy();
-            message.error(msg);
-          }
+          setTimeout(() => {
+            const newFirst = document.querySelector(
+              `.${styles.posts} div:nth-child(${currentLastIndex + 1})`
+            );
+            scrollToElement(newFirst);
+          });
         }
-      })();
+      } catch (err) {
+        setIsFetching(false);
+        const msg = err?.response?.data?.message;
+        if (message && msg) {
+          message.destroy();
+          message.error(msg);
+        }
+      }
     },
     [isFetching, posts.list, posts.page.count]
   );
